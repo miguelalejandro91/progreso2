5fb4d0b4634c7d8fb7ebcd57e9916ce49d6aaeaa
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-16 12:08:43 -0500

    4

--- suma de matrices.c
+++ suma de matrices.c
@@ -1,58 +1,68 @@
 /*Crear un programa en C que permita sumar 2 matrices de la misma dimensión*/
 
+
+
+
 #include <stdio.h>
+
 
 #define MAX_FILAS 100
 #define MAX_COLUMNAS 100
-void sumarMatrices(int matriz1[MAX_FILAS][MAX_COLUMNAS], int matriz2[MAX_FILAS][MAX_COLUMNAS], int resultado[MAX_FILAS][MAX_COLUMNAS], int filas, int columnas) {
+
+// Función para sumar dos matrices
+void sumarMatrices(int matriz1[MAX_FILAS][MAX_COLUMNAS], int matriz2[MAX_FILAS][MAX_COLUMNAS], int filas, int columnas) {
+    int resultado[MAX_FILAS][MAX_COLUMNAS];
     int i, j;
+    
+    // Realizar la suma de las matrices
     for (i = 0; i < filas; i++) {
         for (j = 0; j < columnas; j++) {
             resultado[i][j] = matriz1[i][j] + matriz2[i][j];
         }
     }
-}
-
-void mostrarMatriz(int matriz[MAX_FILAS][MAX_COLUMNAS], int filas, int columnas) {
-    int i, j;
+    
+    // Mostrar la matriz resultante
+    printf("La matriz resultante de la suma es:\n");
     for (i = 0; i < filas; i++) {
         for (j = 0; j < columnas; j++) {
-            printf("%d ", matriz[i][j]);
+            printf("%d ", resultado[i][j]);
         }
         printf("\n");
     }
 }
+
 int main() {
-    int matriz1[MAX_FILAS][MAX_COLUMNAS];
-    int matriz2[MAX_FILAS][MAX_COLUMNAS];
-    int resultado[MAX_FILAS][MAX_COLUMNAS];
+    int matriz1[MAX_FILAS][MAX_COLUMNAS], matriz2[MAX_FILAS][MAX_COLUMNAS];
     int filas, columnas;
-
-    printf("Ingrese el número de filas de las matrices: ");
-    scanf("%d", &filas);
-
-    printf("Ingrese el número de columnas de las matrices: ");
-    scanf("%d", &columnas);
-
-    printf("Ingrese los elementos de la primera matriz:\n");
     int i, j;
+    
+    // Obtener las dimensiones de las matrices
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+    
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
+    
+    // Leer los elementos de la primera matriz
+    printf("Ingrese los elementos de la primera matriz:\n");
     for (i = 0; i < filas; i++) {
         for (j = 0; j < columnas; j++) {
+            printf("Elemento (%d, %d): ", i, j);
             scanf("%d", &matriz1[i][j]);
         }
     }
-
+    
+    // Leer los elementos de la segunda matriz
     printf("Ingrese los elementos de la segunda matriz:\n");
     for (i = 0; i < filas; i++) {
         for (j = 0; j < columnas; j++) {
+            printf("Elemento (%d, %d): ", i, j);
             scanf("%d", &matriz2[i][j]);
         }
     }
-
-    sumarMatrices(matriz1, matriz2, resultado, filas, columnas);
-
-    printf("La suma de las matrices es:\n");
-    mostrarMatriz(resultado, filas, columnas);
-
+    
+    // Llamar a la función para sumar las matrices
+    sumarMatrices(matriz1, matriz2, filas, columnas);
+    
     return 0;
 }
