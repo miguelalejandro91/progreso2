17b7975c6de3adf077e593934a7564e990c00969
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-16 12:00:39 -0500

    3

--- suma de matrices.c
+++ suma de matrices.c
@@ -22,3 +22,37 @@ void mostrarMatriz(int matriz[MAX_FILAS][MAX_COLUMNAS], int filas, int columnas)
         printf("\n");
     }
 }
+int main() {
+    int matriz1[MAX_FILAS][MAX_COLUMNAS];
+    int matriz2[MAX_FILAS][MAX_COLUMNAS];
+    int resultado[MAX_FILAS][MAX_COLUMNAS];
+    int filas, columnas;
+
+    printf("Ingrese el número de filas de las matrices: ");
+    scanf("%d", &filas);
+
+    printf("Ingrese el número de columnas de las matrices: ");
+    scanf("%d", &columnas);
+
+    printf("Ingrese los elementos de la primera matriz:\n");
+    int i, j;
+    for (i = 0; i < filas; i++) {
+        for (j = 0; j < columnas; j++) {
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    printf("Ingrese los elementos de la segunda matriz:\n");
+    for (i = 0; i < filas; i++) {
+        for (j = 0; j < columnas; j++) {
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    sumarMatrices(matriz1, matriz2, resultado, filas, columnas);
+
+    printf("La suma de las matrices es:\n");
+    mostrarMatriz(resultado, filas, columnas);
+
+    return 0;
+}
