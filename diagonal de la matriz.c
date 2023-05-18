a2a39d39e1bfe1bc7ce2c8dfd7c7bd40c3f044b4
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-18 11:51:59 -0500

    4

--- diagonal de la matriz.c
+++ diagonal de la matriz.c
@@ -10,7 +10,7 @@ int main() {
     int matriz[MAX_DIMENSION][MAX_DIMENSION];
     int dimension;
     int i, j;
-    printf("Ingrese la dimensión de la matriz: ");
+    printf("Ingrese la dimension de la matriz: ");
     scanf("%d", &dimension);
 
     // Inicializar la matriz en ceros
@@ -24,4 +24,15 @@ int main() {
     for (i = 0; i < dimension; i++) {
         matriz[i][i] = 1;
     }
+    // Imprimir la matriz
+    printf("Matriz resultante:\n");
+    for (i = 0; i < dimension; i++) {
+        for (j = 0; j < dimension; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
