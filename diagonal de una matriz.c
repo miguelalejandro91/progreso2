320c090ee5b94e2b30825f0fb869359f357d93de
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-18 12:14:58 -0500

    utilizo la variable for para ingresar la diagonal principal con unos y mando a imprimir

--- diagonal de una matriz.c
+++ diagonal de una matriz.c
@@ -17,3 +17,17 @@ int main() {
             matriz[i][j] = 0;
         }
     }
+    // Establecer unos en la diagonal principal
+    for (i = 0; i < dimension; i++) {
+        matriz[i][i] = 1;
+    }// Imprimir la matriz
+    printf("Matriz resultante:\n");
+    for (i = 0; i < dimension; i++) {
+        for (j = 0; j < dimension; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
