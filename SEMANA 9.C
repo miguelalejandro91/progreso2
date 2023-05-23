da78c62f5d67e110cac114ff611c37b6375bbf4d
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-23 12:53:26 -0500

    ejecutamos el main con las filas y columnas

--- SEMANA 9.C
+++ SEMANA 9.C
@@ -47,3 +47,11 @@ void calcularMatrizTranspuesta(int** matriz, int filas, int columnas) {
     }
     free(transpuesta);
 }
+int main() {
+    int filas, columnas;
+
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
