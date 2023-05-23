4fb0f0056ef94e7b7178862356367393bdb5ae00
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-23 11:49:28 -0500

    1

---
+++ arreglo tridimencional.c
@@ -0,0 +1,5 @@
+/* Construir un programa en C. Que sea un arreglo tridimencional 
+con 3 matrices tal que las dos primeras matrices inicialicen en ceros
+y la tercera matriz en unos, el usuario debe definir las dimenciones
+ del arreglo, y que se imprima el programa*/
+ 
