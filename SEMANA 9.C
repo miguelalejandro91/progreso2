53185397d2a425af4ae49692eac34efb69e7baa3
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-23 12:42:54 -0500

    defino las librerias

---
+++ SEMANA 9.C
@@ -0,0 +1,10 @@
+/* Construir un programa en C, en donde el usuario 
+defina las dimensiones de una matriz, el programa llena
+la matriz con numeros aleatorios entre cero y cien, y 
+luego calcule la matriz transpuesta. El programa debe 
+imprimir la matriz original y la matriz transpuesta*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
