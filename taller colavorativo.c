1433cb7789a8484753b5563ebbc927e54c158034
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-27 00:10:12 -0500

    declaracion de base de datos de universidad

--- taller colavorativo.c
+++ taller colavorativo.c
@@ -13,3 +13,18 @@ Escriba un programa que calcule:
 a) El año en que ingresó la mayor cantidad de alumnos a la universidad.
 b) La Carrera que recibió la mayor cantidad de alumnos en el último año.
 c) ¿En qué año la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos?*/
+
+#include <stdio.h>
+
+#define NUM_CARRERAS 7
+#define NUM_ANIOS 5
+
+int main() {
+    // Declaración e inicialización del arreglo de alumnos
+    int alumnos[NUM_ANIOS][NUM_CARRERAS] = {
+        {100, 50, 75, 80, 90, 60, 70},     // Año 1
+        {120, 70, 60, 95, 80, 55, 65},     // Año 2
+        {110, 60, 80, 85, 75, 70, 50},     // Año 3
+        {90, 65, 70, 75, 85, 75, 60},      // Año 4
+        {105, 75, 90, 70, 95, 80, 55}      // Año 5
+    };
