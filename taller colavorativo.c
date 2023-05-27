956b8736f162f3c80afd1cc07c0f44b22e0ab466
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-27 00:05:14 -0500

    problema a resolver

---
+++ taller colavorativo.c
@@ -0,0 +1,15 @@
+/*A continuación, se describe el problema computacional a resolver:
+En una Institución educativa se almacenan datos sobre el número de alumnos que han ingresado 
+a sus diferentes carreras en los dos semestres anuales y en los últimos 5 años. Cada carrera tiene 
+un numero asociado que la identifica:
+1. Ingeniería de Software.
+2. Administración.
+3. Economía.
+4. Relaciones Internacionales.
+5. Matemáticas.
+6. Contabilidad.
+7. Ingeniería Industrial.
+Escriba un programa que calcule:
+a) El año en que ingresó la mayor cantidad de alumnos a la universidad.
+b) La Carrera que recibió la mayor cantidad de alumnos en el último año.
+c) ¿En qué año la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos?*/
