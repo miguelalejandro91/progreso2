c4a95b9740e31b43253dfeaadb8e7f7c76674f0a
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-05-27 21:11:49 -0500

    3

--- direcciones ip.c
+++ direcciones ip.c
@@ -16,7 +16,7 @@ long long combinations(int n, int r) {
 
 int main() {
     char ipBinary[40];
-    printf("Ingrese la dirección IP en binario con puntos: ");
+    printf("Ingrese la direccion IP en binario con puntos: ");
     fgets(ipBinary, sizeof(ipBinary), stdin);
 
     int octets[4];
@@ -28,12 +28,12 @@ int main() {
         i++;
     }
 
-    printf("Dirección IP en decimal: %d.%d.%d.%d\n", octets[0], octets[1], octets[2], octets[3]);
-    printf("Dirección IP en hexadecimal: %02X:%02X:%02X:%02X\n", octets[0], octets[1], octets[2], octets[3]);
+    printf("Direccion IP en decimal: %d.%d.%d.%d\n", octets[0], octets[1], octets[2], octets[3]);
+    printf("Direccion IP en hexadecimal: %02X:%02X:%02X:%02X\n", octets[0], octets[1], octets[2], octets[3]);
 
     int sum = octets[0] + octets[1] + octets[2] + octets[3];
     long long numCombinations = combinations(sum, 4);
-    printf("Número de combinaciones posibles: %lld\n", numCombinations);
+    printf("Numero de combinaciones posibles: %d\n", numCombinations);
 
     return 0;
 }
