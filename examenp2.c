93c3ec5c92a8bad4b53995a9ac143c62924b303d
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-06-06 13:04:17 -0500

    datos

---
+++ examenp2.c
@@ -0,0 +1,16 @@
+/*Cada semestre, a la materia de Programación se inscriben una cantidad constante de 
+23 alumnos. El semestre está dividido en 3 progresos. Utilice los conceptos de Arreglos 
+y Funciones para escribir un programa en lenguaje C que permita realizar los siguientes cálculos:
+1- Obtener el promedio de calificaciones de cada alumno durante el semeste.
+2- La nota promedio del grupo de estudiantes para cada progreso
+3- El alumno que obtuvo el mayor promedio de calificación durante el semestre*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+
+
