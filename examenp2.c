4a44f5a21dd77a302ef52c65c307fa4f7efac54f
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-06-06 13:11:49 -0500

    crecion de las variables y llamar a la funcion main

--- examenp2.c
+++ examenp2.c
@@ -25,7 +25,39 @@ float calcularPromedioAlumno(int calificaciones[], int numCalificaciones) {
     return (float)suma / numCalificaciones;
 }
 
+float calcularPromedioGrupo(struct Alumno alumnos[]) {
+    float suma = 0;
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        suma += calcularPromedioAlumno(alumnos[i].calificaciones, NUM_PROGRESOS);
+    }
+    return suma / NUM_ALUMNOS;
+}
 
+int encontrarMejorAlumno(struct Alumno alumnos[]) {
+    int mejorAlumno = 0;
+    float mejorPromedio = calcularPromedioAlumno(alumnos[0].calificaciones, NUM_PROGRESOS);
+
+    for (int i = 1; i < NUM_ALUMNOS; i++) {
+        float promedio = calcularPromedioAlumno(alumnos[i].calificaciones, NUM_PROGRESOS);
+        if (promedio > mejorPromedio) {
+            mejorPromedio = promedio;
+            mejorAlumno = i;
+        }
+    }
+
+    return mejorAlumno;
+}
+
+int main() {
+    struct Alumno alumnos[NUM_ALUMNOS];
+    srand(time(NULL));
+
+    // Inicializar los nombres de los alumnos (ejemplo)
+    char nombres[NUM_ALUMNOS][50] = {
+        "Juan",
+        "Maria",
+        "Pedro",
+        // Agregar más nombres
 
 
 
